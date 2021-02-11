4ff933f322bbbc7eb4e3685eb1cccdba2589c645
Mike Ajala <agmt5989@gmail.com>
2021-02-11 18:29:31 +0100

    Echoed out a few descriptive lines to the user

--- docker.sh
+++ docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 # This script provisions 3 docker containers
+
+echo "This is a script that automatically provisions 3 docker containers"
+echo "The containers will respectively contain kibana, nginx and mysql instances"
