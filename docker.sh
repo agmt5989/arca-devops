c16a50cb7833e0963112f711a4dfda4729a57a3b
Mike Ajala <agmt5989@gmail.com>
2021-02-12 07:44:49 +0100

    added commands to pull the three respective images from docker hub

--- docker.sh
+++ docker.sh
@@ -4,3 +4,10 @@
 
 echo "This is a script that automatically provisions 3 docker containers"
 echo "The containers will respectively contain kibana, nginx and mysql instances"
+
+# The docker commands
+docker pull kibana:6.4.2
+docker pull nginx
+docker pull mysql
+
+echo "Successfully pulled container images from the docker public registry"
