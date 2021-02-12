f743da5a4103aeaa7833b862de926eabd1a55dc8
Mike Ajala <agmt5989@gmail.com>
2021-02-12 08:36:19 +0100

    Actually running the docker containers from the images

--- docker.sh
+++ docker.sh
@@ -11,3 +11,11 @@ docker pull nginx
 docker pull mysql
 
 echo "Successfully pulled container images from the docker public registry"
+
+# Next, we create containers from the respective images, 
+# making sure to detatch in interactive mode, 
+# and installing iputils-ping as the case applies.
+docker run -dit --name kibana kibana:6.4.2 /bin/bash # kibana comes with ping installed
+docker run -dit --name nginx nginx /bin/bash
+docker run -dit --name mysql mysql /bin/bash
+
