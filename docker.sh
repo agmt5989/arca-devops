d6714930be29b3bdbc10b4a0c40aeb12d43e8931
Mike Ajala <agmt5989@gmail.com>
2021-02-12 10:00:30 +0100

    Installed iputils-ping to make sure the ping command is available in all 3 containers

--- docker.sh
+++ docker.sh
@@ -6,6 +6,7 @@ echo "This is a script that automatically provisions 3 docker containers"
 echo "The containers will respectively contain kibana, nginx and mysql instances"
 
 # The docker commands
+
 docker pull kibana:6.4.2
 docker pull nginx
 docker pull mysql
@@ -14,8 +15,13 @@ echo "Successfully pulled container images from the docker public registry"
 
 # Next, we create containers from the respective images, 
 # making sure to detatch in interactive mode, 
-# and installing iputils-ping as the case applies.
-docker run -dit --name kibana kibana:6.4.2 /bin/bash # kibana comes with ping installed
+
+docker run -dit --name kibana kibana:6.4.2 /bin/bash 
 docker run -dit --name nginx nginx /bin/bash
 docker run -dit --name mysql mysql /bin/bash
+
+# Installing iputils-ping as the case applies.
+# kibana comes with ping installed
+docker exec nginx bash -c "apt update ; apt -y install iputils-ping"
+docker exec mysql bash -c "apt update ; apt -y install iputils-ping"
 
